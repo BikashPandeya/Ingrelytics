812c44a6d56303e2edf78421ad875425a174709f
BikashPandeya <bikashpandeya5@gmail.com>
2025-11-12 11:16:04 +0545

    Created header and footer  using tailwind css built in model and daisy ui build in model

--- ingredient-insight/app/layout.tsx
+++ ingredient-insight/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Example from "@/components/header";
+import Footer from "@/components/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,15 +21,15 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <Example />
         {children}
+        <Footer />
       </body>
     </html>
   );
---
+++ ingredient-insight/components/header.tsx
@@ -0,0 +1,112 @@
+import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+
+const navigation = [
+  { name: 'Dashboard', href: '#', current: true },
+  { name: 'Team', href: '#', current: false },
+  { name: 'Projects', href: '#', current: false },
+  { name: 'Calendar', href: '#', current: false },
+]
+
+function classNames(...classes : (string | undefined | false)[]) {
+  return classes.filter(Boolean).join(' ')
+}
+
+export default function Example() {
+  return (
+    <Disclosure
+      as="nav"
+      className="relative bg-gray-800/50 text-lg after:pointer-events-none after:absolute after:inset-x-0 after:bottom-0 after:h-px after:bg-white/10"
+    >
+      <div className="mx-auto max-w-7xl px-4 sm:px-8 lg:px-12">
+        <div className="relative flex h-20 items-center justify-between">
+          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+            <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-3 text-gray-400 hover:bg-white/5 hover:text-white focus:outline-2 focus:-outline-offset-1 focus:outline-indigo-500">
+              <span className="absolute -inset-0.5" />
+              <span className="sr-only">Open main menu</span>
+              <Bars3Icon aria-hidden="true" className="block size-8 group-data-open:hidden" />
+              <XMarkIcon aria-hidden="true" className="hidden size-8 group-data-open:block" />
+            </DisclosureButton>
+          </div>
+          <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+            <div className="flex shrink-0 items-center">
+              <img
+                alt="Ingredients Logo"
+                src="/logo.png"
+                className="h-14 w-auto sm:h-16  "
+              />
+            </div>
+            <div className="hidden sm:ml-8 sm:block">
+              <div className="flex space-x-6">
+                {navigation.map((item) => (
+                  <a
+                    key={item.name}
+                    href={item.href}
+                    aria-current={item.current ? 'page' : undefined}
+                    className={classNames(
+                      item.current ? 'bg-gray-950/50 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white',
+                      'rounded-md px-5 py-3 text-base font-semibold',
+                    )}
+                  >
+                    {item.name}
+                  </a>
+                ))}
+              </div>
+            </div>
+          </div>
+          <div className="absolute inset-y-0 right-0 flex items-center pr-3 sm:static sm:inset-auto sm:ml-8 sm:pr-0">
+            <button
+              type="button"
+              className="relative rounded-full p-2 text-gray-400 hover:text-white focus:outline-2 focus:outline-offset-2 focus:outline-indigo-500"
+            >
+              <span className="absolute -inset-1.5" />
+              <BellIcon aria-hidden="true" className="size-8" />
+            </button>
+            <Menu as="div" className="relative ml-4">
+              <MenuButton className="relative flex rounded-full focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">
+                <span className="absolute -inset-1.5" />
+                <img
+                  alt=""
+                  src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                  className="size-10 rounded-full bg-gray-800 outline -outline-offset-1 outline-white/10"
+                />
+              </MenuButton>
+              <MenuItems
+                transition
+                className="absolute right-0 z-10 mt-3 w-56 origin-top-right rounded-md bg-gray-800 py-1 outline -outline-offset-1 outline-white/10 transition data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
+              >
+                <MenuItem>
+                  <a href="#" className="block px-5 py-3 text-base text-gray-300 data-focus:bg-white/5 data-focus:outline-hidden">Your profile</a>
+                </MenuItem>
+                <MenuItem>
+                  <a href="#" className="block px-5 py-3 text-base text-gray-300 data-focus:bg-white/5 data-focus:outline-hidden">Settings</a>
+                </MenuItem>
+                <MenuItem>
+                  <a href="#" className="block px-5 py-3 text-base text-gray-300 data-focus:bg-white/5 data-focus:outline-hidden">Sign out</a>
+                </MenuItem>
+              </MenuItems>
+            </Menu>
+          </div>
+        </div>
+      </div>
+      <DisclosurePanel className="sm:hidden">
+        <div className="space-y-1 px-3 pt-3 pb-4">
+          {navigation.map((item) => (
+            <DisclosureButton
+              key={item.name}
+              as="a"
+              href={item.href}
+              aria-current={item.current ? 'page' : undefined}
+              className={classNames(
+                item.current ? 'bg-gray-950/50 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white',
+                'block rounded-md px-4 py-3 text-lg font-semibold',
+              )}
+            >
+              {item.name}
+            </DisclosureButton>
+          ))}
+        </div>
+      </DisclosurePanel>
+    </Disclosure>
+  )
+}
